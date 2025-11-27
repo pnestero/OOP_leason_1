d55cfae742864b6f13ee1d207130296d3b0f1d9d
Pavel Nesterov <pnestero@gmail.com>
2025-11-28 00:29:40 +0500

    final version

---
+++ tests/test_products.py
@@ -0,0 +1,6 @@
+def test_product(tests1_product):
+    assert tests1_product.name == "Банан"
+    assert tests1_product.description == "Чтобы сесть"
+    assert tests1_product.price == 123.12
+    assert tests1_product.quantity == 2
+
