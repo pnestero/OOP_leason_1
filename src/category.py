aab34a6b42b31f10d5b496ecdbbe9f2894df31ee
Pavel Nesterov <pnestero@gmail.com>
2025-12-06 17:40:54 +0500

    final version_2

--- src/category.py
+++ src/category.py
@@ -1,34 +1,45 @@
 from src.products import Product
 
 class Category:
-    name = str
-    description = str
-    products = list[Product]
+    name: str
+    description: str
+    __products: list[Product]
     count_categories = 0
     count_products = 0
 
-    def __init__(self, name, description, products):
+    def __init__(self, name, description, init_products):
+        """Инициализация класса"""
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = init_products if init_products else []
         Category.count_categories += 1
-        Category.count_products += len(products) if products else 0
+        Category.count_products += len(self.__products)
+
+    def add_product(self, product):
+        """Добавление в класс Category списка продуктов """
+        if isinstance(product, Product):
+            self.__products.append(product)
+            Category.count_products += 1
+
+    @property
+    def get_products(self):
+        return self.__products
+
+    @property
+    def products(self):
+        product_strings = []
+        for product in self.__products:
+            product_strings.append(f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт.")
+        return "\n".join(product_strings)
 
 
 # if __name__ == "__main__":
-#     prod1 = Product("Банан", "Чтобы сесть", "123.12", "1")
-#     prod2 = Product("Ложка", "Чтобы есть", "10000", "20")
+#     products = Product(
+#     "Смартфоны",
+#     "Смартфоны, как средство не только коммуникации, но и получение дополнительных функций для удобства жизни",
+#     "Samsung Galaxy C23 Ultra",
+#     "121")
 #
-#     category = Category("banan", "sitdown", ["sss", "aaa", "ddd", "fjfdg"])
-#
-#     print(category.name)
-#     print(category.description)
-#     print(category.products)
-#     print(len(category.products))
-#     print("=" * 50)
-#     print(category.count_products)
-#     print(category.count_categories)
-    # print(prod.name)
-    # print(prod.description)
-    # print(prod.price)
-    # print(prod.quantity)
+#     print(Category.description)
+#     print(Category.name)
+#     print(Category.products)
